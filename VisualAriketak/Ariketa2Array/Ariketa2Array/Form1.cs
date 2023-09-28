83c625c21e64f6b8d0684d238f86c7b30f188cad
Arruabarrena Area <arruabarrena.aritz@uni.lan>
2023-09-28 14:25:19 +0200

    asdfasf1231adaad

--- VisualAriketak/Ariketa2Array/Ariketa2Array/Form1.cs
+++ VisualAriketak/Ariketa2Array/Ariketa2Array/Form1.cs
@@ -16,27 +16,27 @@ namespace Ariketa2Array
             {
                 case 0:
                     label1.Text = "Zenbakia 2";
-                    zenbakiak[0] = float.Parse(textBox1.Text);
+                    zenbakia.Zenbakiak[0] = float.Parse(textBox1.Text);
                     textBox1.Clear();
                     i = 1;
                     break;
                 case 1:
                     label1.Text = "Zenbakia 3";
-                    zenbakiak[1] = float.Parse(textBox1.Text);
+                    zenbakia.Zenbakiak[1] = float.Parse(textBox1.Text);
                     textBox1.Clear();
                     i = 2;
                     break;
                 case 2:
                     label1.Text = "Zenbakia 4";
-                    zenbakiak[2] = float.Parse(textBox1.Text);
+                    zenbakia.Zenbakiak[2] = float.Parse(textBox1.Text);
                     textBox1.Clear();
                     i = 3;
                     break;
                 case 3:
                     label1.Text = "Emaitza";
-                    zenbakiak[3] = float.Parse(textBox1.Text);
+                    zenbakia.Zenbakiak[3] = float.Parse(textBox1.Text);
                     zenbakia.eragiketaluzea();
-                    textBox1.Text = "(" + zenbakiak[0] + " + (" + zenbakiak[1] + "x2)" + " + (" + zenbakiak[2] + "x3)" + " + (" + zenbakiak[3] + "x4)/4 = " + zenbakia.eragiketa.ToString();
+                    textBox1.Text = "(" + zenbakia.Zenbakiak[0] + " + (" + zenbakia.Zenbakiak[1] + "x2)" + " + (" + zenbakia.Zenbakiak[2] + "x3)" + " + (" + zenbakia.Zenbakiak[3] + "x4)/4 = " + zenbakia.eragiketa.ToString();
                     i++;
                     break;
             }
---
+++ VisualAriketak/Ariketa2List/Ariketa2List/Ariketa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariketa2List
+{
+    internal class Ariketa
+    {
+
+        public string Label { get; set; }
+        public double Zenbakia { get; set; }
+        public Ariketa(double zenbakia)
+        {
+            this.Zenbakia = zenbakia;
+        }
+
+
+        public static double Eragiketa(List<Ariketa> objetuak)
+        {
+            double emaitza = (objetuak[0].Zenbakia + (2 * objetuak[1].Zenbakia) + (3 * objetuak[2].Zenbakia) + (4 * objetuak[3].Zenbakia)) / 4;
+            return emaitza;
+        }
+    }
+}
